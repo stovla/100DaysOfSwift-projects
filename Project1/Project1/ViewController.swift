a9ee3ce10055035a9d6c16659121ab204db70d2c
Vlastimir <vlastimirsd@gmail.com>
2019-02-22 08:40:00 +0100

    project 1 started

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Vlastimir Radojevic on 22/2/19.
+//  Copyright © 2019 Vlastimir Radojevic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fileManager = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fileManager.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // this is a picture load!
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+
+}
+
