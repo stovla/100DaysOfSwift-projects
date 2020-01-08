1be7e3d652bf49bc599253cfe16f2dc3c0bc5e26
Vlastimir <vlastimirsd@gmail.com>
2020-01-08 06:46:34 +0100

    started Milestone 3
    Projects 7-9

---
+++ Milestone3/Milestone3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Milestone3
+//
+//  Created by Vlastimir on 07/01/2020.
+//  Copyright © 2020 Vlastimir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
